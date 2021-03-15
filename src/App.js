ac376023d6a8a582ddcde422734b5ec5e8e0496a
Serj <miner_x@bk.ru>
2021-03-15 12:30:26 +0300

    add env

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
-import './App.css';
-import {ApolloClient, ApolloLink, InMemoryCache, HttpLink} from 'apollo-boost'
-import {ApolloProvider} from 'react-apollo'
-import Month from './components/Month/Month'
-import {BrowserRouter, Route, Link} from 'react-router-dom'
-import AddTransaction from './components/AddTransaction/AddTransaction'
+import "./App.css";
+import {
+  ApolloClient,
+  ApolloLink,
+  InMemoryCache,
+  HttpLink,
+} from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import Month from "./components/Month/Month";
+import { BrowserRouter, Route } from "react-router-dom";
+import AddTransaction from "./components/AddTransaction/AddTransaction";
 
-const httpLink = new HttpLink({ uri: 'https://minaev.pw/works/test-wp/graphql' });
+const AuthHeaders = process.env.REACT_APP_AUTHORIZATION;
+
+const httpLink = new HttpLink({
+  uri: "https://minaev.pw/works/test-wp/graphql",
+});
 
 const authLink = new ApolloLink((operation, forward) => {
-    operation.setContext({
-        headers: {
-            authorization: 'Basic YWRtaW46cTF3MmUzcjQ='
-        }
-    });
-    return forward(operation);
+  operation.setContext({
+    headers: {
+      authorization: AuthHeaders,
+    },
+  });
+  return forward(operation);
 });
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
-    cache: new InMemoryCache()
-})
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-      <ApolloProvider client={client}>
-          <BrowserRouter>
-              <div className="App">
-                  <Route exact path='/' component={Month} />
-                  <Route exact path='/add' component={AddTransaction} />
-              </div>
-          </BrowserRouter>
-      </ApolloProvider>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <div className="App">
+          <Route exact path="/" component={Month} />
+          <Route exact path="/add" component={AddTransaction} />
+        </div>
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
 
